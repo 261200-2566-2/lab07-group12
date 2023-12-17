8f0d8471708878409390505055bd476a9bf73e2d
pubest <pubestpubest@gmail.com>
2023-12-17 23:14:39 +0700

    all done needed to be test

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,26 @@
+import java.util.HashMap;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Set<String> a = new Set<String>();
+        Set<String> b = new Set<String>();
+        a.add("aa");
+        a.add("bb");
+        a.add("cc");
+        b.add("bb");
+        b.add("cc");
+        b.add("aa");
+//        b.add("gg");
+//        System.out.println(a.containsAll(b)?"Yes":"No");
+//        System.out.println(a.removeAll(b)?"Yes":"No");
+//        System.out.println(a.size());
+//        System.out.println(a.removeAll(b)?"Yes":"No");
+//        System.out.println(a.addAll(b)?"Yes":"No");
+        System.out.println(a.retainAll(b)?"Yes":"No");
+        System.out.println(a.size());
+        a.clear();
+        System.out.println(a.size());
     }
 }
